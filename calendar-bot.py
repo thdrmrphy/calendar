1083ff9ad7f0d12ced6df2977027fef9e3a9d5ec
Theodore Murphy <130824397+thdrmrphy@users.noreply.github.com>
2024-06-04 19:26:47 +1000

    Update to accomodate events without Challenge Areas

--- calendar-bot.py
+++ calendar-bot.py
@@ -100,6 +100,7 @@ def format_challenge(challenge_area):
         "outdoors": "🏕️ Outdoor Challenge",
         "creative": "💡 Creative Challenge",
         "personal_growth": "🌱 Personal Growth Challenge",
+        "not_applicable": "Does not count towards Milestones"
     }
     challenge_replace = challenge.get(challenge_area, challenge_area)
 
